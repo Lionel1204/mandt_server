86f2de1d32b5f83e4d402a86ab25a4335b12c8f3
Lionel Shen <lionel.shen@autodesk.com>
2022-07-10 11:59:17 +0800

    Added the qrcode api

--- controllers/qrController.js
+++ controllers/qrController.js
@@ -2,18 +2,18 @@ const BaseController = require('./baseController');
 const qr = require('qrcode');
 
 class QrController extends BaseController {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    async post(req, res) {
-        const { content } = req.body;
-        qr.toDataURL(content, (err, result) => {
-            if (err) res.send("Generating QRCode Error");
-            res.send(result);
-        });
-    }
+  async post(req, res) {
+    const { content } = req.body;
+    const opts = {type: 'png'};
+    qr.toDataURL(content, opts,(err, result) => {
+      if (err) res.send('Generating QRCode Error');
+      res.send(result);
+    });
+  }
 }
 
 module.exports = QrController;
-
